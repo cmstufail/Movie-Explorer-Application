3fe30410e384ad95096fe53dac5c2343909c5dc8
cmstufail <cmstufail@gmail.com>
2026-09-17 04:33:43 +0600

    second commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -131,7 +131,7 @@ export default function HomePage({
 
           {/* Grid Loading Skeletons */}
           {loading ? (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
               {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((n) => (
                 <div
                   key={n}
